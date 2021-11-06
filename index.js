92bd966ef97f604738dd1c90468e5634fd274853
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-11-06 18:39:47 +0530

    initial requirements

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const router = require('./productroutes')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/', router)
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
